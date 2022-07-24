3b1ca4aade5611404c43e7c28996968247b77c76
NaotoFujihiro <naoto.fujihiro@gmail.com>
2022-07-24 13:22:17 +0900

    Create install.sh to download the repository

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR_INSTALL="${DIR_INSTALL:-$HOME/repos/github.com/NaotoFujihiro/dotfiles}"
+
+if [ -d "$DIR_INSTALL" ]; then
+    echo "Updating the installed dotfiles...";
+    cd "$DIR_INSTALL" | git pull --rebase git@github.com:NaotoFujihiro/dotfiles.git
+else
+    echo "Installing dotfiles...";
+    git clone git@github.com:NaotoFujihiro/dotfiles.git "$DIR_INSTALL"
+fi
+
+/bin/bash "$DIR_INSTALL/scripts/setup.bash"

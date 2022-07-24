8f3d7519934a235118a974144d0bf8772922b35c
NaotoFujihiro <naoto.fujihiro@gmail.com>
2022-07-24 23:31:38 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ DIR_INSTALL="${DIR_INSTALL:-$HOME/repos/github.com/NaotoFujihiro/dotfiles}"
 
 if [ -d "$DIR_INSTALL" ]; then
     echo "Updating the installed dotfiles...";
-    cd "$DIR_INSTALL" | git pull --rebase git@github.com:NaotoFujihiro/dotfiles.git
+    (
+        cd "$DIR_INSTALL"
+        git pull --ff-only "git@github.com:NaotoFujihiro/dotfiles.git"
+    )
 else
     echo "Installing dotfiles...";
-    git clone git@github.com:NaotoFujihiro/dotfiles.git "$DIR_INSTALL"
+    git clone "git@github.com:NaotoFujihiro/dotfiles.git" "$DIR_INSTALL"
 fi
 
 /bin/bash "$DIR_INSTALL/scripts/setup.bash"
